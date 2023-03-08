1249b8ae4c9f2367493f82554a62edcb653a8a3d
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-03-08 19:37:33 +0530

    aws parameter store

--- aws-parameters/state.tf
+++ aws-parameters/state.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {}
 }
-resource "aws_ssm_parameter" "parameters" {
+
+resource "aws_ssm_parameter" "parameter" {
   count = length(var.parameters)
   name  = var.parameters[count.index].name
   type  = var.parameters[count.index].type
