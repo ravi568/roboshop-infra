dc5fe7c8452e32c53cf83a185510243e03d86bcc
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-03-09 19:34:09 +0530

    all components inside main.tfvars inside roboshop infra

--- ec2/main.tf
+++ ec2/main.tf
@@ -19,7 +19,7 @@ resource "null_resource" "provisioner" {
     }
 
     inline = [
-      "ansible-pull -i localhost, -U https://github.com/ravi568/roboshop-ansible roboshop.yml -e role_name=${var.component}"
+      "ansible-pull -i localhost, -U https://github.com/ravi568/roboshop-ansible roboshop.yml -e role_name=${var.component} -e env=${var.env}"
     ]
 
   }
