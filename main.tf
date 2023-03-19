2893694e0a8963ef250fad7134b507e1635c9879
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-03-19 21:44:30 +0530

    vpc creation for network setup

--- main.tf
+++ main.tf
@@ -5,5 +5,7 @@ module "vpc" {
   tags = var.tags
   for_each = var.vpc
   vpc_cidr = each.value["vpc_cidr"]
+  public_subnets = each.value["public_subnets"]
+  private_subnets = each.value["private_subnets"]
 
 }
