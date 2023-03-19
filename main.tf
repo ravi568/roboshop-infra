c9b704894be66594f9c4efd1b0bb9b473cc6a671
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-03-19 21:49:24 +0530

    vpc creation for network setup

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "vpc" {
-
   source = "git::https://github.com/ravi568/tf-module-vpc.git"
   env = var.env
   tags = var.tags
+
   for_each = var.vpc
   vpc_cidr = each.value["vpc_cidr"]
   public_subnets = each.value["public_subnets"]
