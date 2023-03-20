eca2fe78938535422c997f06c52a43c065bf4c43
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-03-20 20:24:03 +0530

    igw in public subnet

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@ module "vpc" {
   source = "git::https://github.com/ravi568/tf-module-vpc.git"
   env = var.env
   tags = var.tags
+  default_vpc_id= var.default_vpc_id
+  default_route_table= var.default_route_table
 
   for_each = var.vpc
   vpc_cidr = each.value["vpc_cidr"]
--- terraform.tfvars
+++ terraform.tfvars
@@ -4,3 +4,6 @@ tags = {
   owner        = "ecom_bu"
   cost_center  = 1001
 }
+
+default_vpc_id= "vpc-02eb67d0f46661b91"
+default_route_table= "rtb-08650d2224cc0bb87"
