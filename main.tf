1ec65653050300107ee97149eba12ad6c88df739
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-03-25 00:40:15 +0530

    docdb with subnet group, instance inside cluster

--- main.tf
+++ main.tf
@@ -12,6 +12,27 @@ module "vpc" {
 
 }
 
+
+
+module "docdb" {
+  source = "git::https://github.com/ravi568/tf-module-docdb.git"
+  env = var.env
+  tags = var.tags
+  # subnet id is cmoing from vpc module private subnets to here as input
+  subnet_ids= local.db_subnet_ids
+
+  for_each = var.docdb
+  engine = each.value["docdb"]
+  engine_version= each.value["engine_version"]
+  backup_retention_period= each.value["backup_retention_period"]
+  preferred_backup_window= each.value["preferred_backup_window"]
+  skip_final_snapshot= each.value["skip_final_snapshot"]
+  no_of_instances= each.value["no_of_instances"]
+  instance_class= each.value["instance_class"]
+
+}
+
+
 #output "vpc" {
 #  value = module.vpc
 #}
