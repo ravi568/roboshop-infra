9956c3e45e820173ecb07bf1673dfaad92d56a9d
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-03-29 23:54:01 +0530

    components to docdb, elasticache etcc..

--- main.tf
+++ main.tf
@@ -13,26 +13,26 @@ module "vpc" {
 }
 
 
-#module "docdb" {
-#  source = "git::https://github.com/ravi568/tf-module-docdb.git"
-#  env = var.env
-#  tags = var.tags
-#
-#  # subnet id is cmoing from vpc module private subnets to here as input
-#  subnet_ids= local.db_subnet_ids
-#  vpc_id= module.vpc["main"].vpc_id
-#
-#  for_each = var.docdb
-#  engine = each.value["engine"]
-#  engine_version= each.value["engine_version"]
-#  backup_retention_period= each.value["backup_retention_period"]
-#  preferred_backup_window= each.value["preferred_backup_window"]
-#  skip_final_snapshot= each.value["skip_final_snapshot"]
-#  no_of_instances= each.value["no_of_instances"]
-#  instance_class= each.value["instance_class"]
-#  allow_subnets= lookup(local.subnet_ids,each.value["allow_subnets"],null)
-#
-#}
+module "docdb" {
+  source = "git::https://github.com/ravi568/tf-module-docdb.git"
+  env = var.env
+  tags = var.tags
+
+  # subnet id is cmoing from vpc module private subnets to here as input
+  subnet_ids= local.db_subnet_ids
+  vpc_id= module.vpc["main"].vpc_id
+
+  for_each = var.docdb
+  engine = each.value["engine"]
+  engine_version= each.value["engine_version"]
+  backup_retention_period= each.value["backup_retention_period"]
+  preferred_backup_window= each.value["preferred_backup_window"]
+  skip_final_snapshot= each.value["skip_final_snapshot"]
+  no_of_instances= each.value["no_of_instances"]
+  instance_class= each.value["instance_class"]
+  allow_subnets= lookup(local.subnet_cidr,each.value["allow_subnets"],null)
+
+}
 
 module "rds"{
   source = "git::https://github.com/ravi568/tf-module-rds.git"
